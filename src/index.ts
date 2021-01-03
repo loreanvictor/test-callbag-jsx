54f0e41f31bbc184c869df0825f19e0087951eb1
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-01-03 20:23:11 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { DOMWindow, JSDOM } from 'jsdom';
+import { LiveDOMRenderer } from 'render-jsx/dom';
+import { makeRenderer } from 'callbag-jsx';
+
+
+export interface Extras {
+  window: DOMWindow;
+  click: (element: HTMLElement) => void;
+}
+
+export function testRender(test: (renderer: LiveDOMRenderer, document: Document, extras: Extras) => void) {
+  const dom = new JSDOM().window;
+  const renderer = makeRenderer(dom);
+
+  test(renderer, dom.document, {
+    window: dom,
+    click: el => {
+      const e = new dom.Event('click');
+      el.dispatchEvent(e);
+    }
+  });
+}
